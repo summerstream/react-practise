aac5df7c01dde7ac6e7b1a85ade01d593279ba23
culnoty <culnoty@outlook.com>
2017-09-24 10:14:02 +0800

    remove unnecessary store

--- src/components/geeksearch/geek-search.js
+++ src/components/geeksearch/geek-search.js
@@ -1,28 +1,12 @@
 import React from 'react'
-import {createStore,applyMiddleware} from 'redux'
-import {Provider} from 'react-redux'
-import thunkMiddleware from 'redux-thunk'
-import {createLogger} from 'redux-logger'
 import SearchBarContainer from '../../containers/search-bar-container'
 import SearchResultsContainer from '../../containers/search-results-container'
-import reducers from '../../reducers/geek-search'
-
-const loggerMiddleware = createLogger()
-
-const store = createStore(reducers,
-    applyMiddleware(
-        thunkMiddleware,
-        loggerMiddleware
-    )
-)
 
 const GeekSearch = ()=>(
-    <Provider store={store}>
         <div>
             <SearchBarContainer />
             <SearchResultsContainer />
         </div>
-    </Provider>
 )
 
 export default GeekSearch
